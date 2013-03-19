8fd4d4c6a32946ed2fb78f01420c80e8fe342df9
Pavel Aksenkin <pa@monosnap.com>
2013-03-19 01:23:11 -0700

    Initial Commit

---
+++ 3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  3
+//
+//  Created by Pavel Aksenkin on 19.03.13.
+//  Copyright (c) 2013 Pavel Aksenkin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ 3/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  3
+//
+//  Created by Pavel Aksenkin on 19.03.13.
+//  Copyright (c) 2013 Pavel Aksenkin. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
